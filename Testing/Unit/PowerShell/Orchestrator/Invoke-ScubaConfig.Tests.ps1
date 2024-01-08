abd8fcf31acfee66c41f1d7a8948576ab2f52e5c
Ken Palmer <kpalmer@mitre.org>
2024-01-08 15:19:05 -0500

    Simplify pester tests so it does not use another module

--- Testing/Unit/PowerShell/Orchestrator/Invoke-ScubaConfig.Tests.ps1
+++ Testing/Unit/PowerShell/Orchestrator/Invoke-ScubaConfig.Tests.ps1
@@ -4,15 +4,7 @@ $ConnectionPath = '../../../../PowerShell/ScubaGear/Modules/Connection/Connectio
 
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath $OrchestratorPath) -Function 'Invoke-SCuBA' -Force
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath $ScubaConfigPath)
-
 Import-Module (Join-Path -Path $PSScriptRoot -ChildPath $ConnectionPath) -Function Disconnect-SCuBATenant
-
-# This construct did not work
-# . (Join-Path -Path $PSScriptRoot -ChildPath '../PsTestUtils.ps1')
-# Using this instead
-$TestUtilsPath = "../PsTestUtils.psm1"
-Import-Module (Join-Path -Path $PSScriptRoot -ChildPath $TestUtilsPath)
-
 
 InModuleScope Orchestrator {
     Describe -Tag 'Orchestrator' -Name 'Invoke-Scuba with Config' {
@@ -37,55 +29,6 @@ InModuleScope Orchestrator {
             Mock -CommandName New-Item {}
             Mock -CommandName Copy-Item {}
         }
-        Context 'Test Compare-Hash utility used by Invoke-Scuba -ConfigFilePath tests' {
-            BeforeAll {
-            }
-            It 'Test Compare-Hash missing key in first hash' {
-                $a = @{a='a'}
-                $b = @{a='a';b='b'}
-                [array]$ret = Compare-Hash $a $b
-                $ret[0] | Should -BeExactly 'hash1 missing key: b'
-                $ret[1] | Should -BeFalse
-            }
-            It 'Test Compare-Hashes missing key in second hash' {
-                $a = @{a='a';b='b'}
-                $b = @{a='a'}
-                [array]$ret = Compare-Hash $a $b
-                $ret[0] | Should -BeExactly 'hash2 missing key: b'
-                $ret[1] | Select-Object -Index 1 | Should -BeFalse
-            }
-            It 'Test Compare-Hash different keys by 1' {
-                $a = @{a='a'}
-                $b = @{b='b'}
-                [array]$ret = Compare-Hash $a $b
-                $ret[0] | Should -BeExactly 'hash2 missing key: a'
-                $ret[1] | Should -BeExactly 'hash1 missing key: b'
-                $ret[2] | Should -BeFalse
-            }
-            It 'Test Compare-Hash multiple different keys' {
-                $a = @{a='a'; b='b'; c='c'}
-                $b = @{a='a'; y='y'; z='z'}
-                [array]$ret = Compare-Hash $a $b
-                $ret[0] | Should -Bein 'hash2 missing key: c b','hash2 missing key: b c'
-                $ret[1] | Should -BeIn 'hash1 missing key: y z','hash1 missing key: z y'
-                $ret[2] | Should -BeFalse
-            }
-            It 'Test Compare-Hash different values' {
-                $a = @{a='b'}
-                $b = @{a='a'}
-                [array]$ret = Compare-Hash $a $b
-                $ret[0]| Should -BeExactly 'key a values differ b and a'
-                $ret[1] | Select-Object -Index 1  | Should -BeFalse
-            }
-            It 'Test Compare-Hash multiple differrent values' {
-                $a = @{a='a';b='b'}
-                $b = @{a='y';b='z'}
-                [array]$ret = Compare-Hash $a $b
-                $ret[0] | Should -BeExactly 'key a values differ a and y'
-                $ret[1] | Should -BeExactly 'key b values differ b and z'
-                $ret[2] | Select-Object -Index 1  | Should -BeFalse
-            }
-        }
         Context 'Testing Invoke-Scuba with -ConfigFilePath arg and parameter override' {
             BeforeAll {
                 $ConfigFile = ( Join-Path -Path $PSScriptRoot  -ChildPath "orchestrator_config_test.yaml" )
@@ -101,14 +44,31 @@ InModuleScope Orchestrator {
                     $SplatParams[$ModKey] = $ModValue
                     Invoke-Scuba @SplatParams
                     $ConfTest = [ScubaConfig]::GetInstance().Configuration
-                    # Verify there is a difference with the override
-                    $retTestForFalse = Compare-Hash $ScubaConfRef $ConfTest | Select-Object -Last 1
-                    # Verify there are no other differences
-                    $ConfTest[$Modkey] = $ScubaConfRef[$Modkey]
-                    $retTestForTrue = Compare-Hash $ScubaConfRef $ConfTest |  Select-Object -Last 1
-                    return ( -Not ( $retTestForFalse ) -and ( $retTestForTrue ))
+                    $pass = $true
+                    if ( (Compare-Object @($ScubaConfRef.keys) @($ConfTest.keys)))
+                    {
+                        $pass = $false
+                    }
+                    else
+                    {
+                        # Test all values to be sure that the override parameter matches modified value
+                        # The other values should be the same
+                        foreach ($key in $ConfTest.keys )
+                        {
+                            if ( $key -eq $Modkey ) {
+                                $isDifferentValue = (( Compare-Object  $ModValue $ConfTest.$key ) -ne $none )
+                            }
+                            else {
+                                $isDifferentValue =  (( Compare-Object  $ScubaConfRef.$key $ConfTest.$key ) -ne $none )<# Action when all if and elseif conditions are false #>
+                            }
+                        }
+                        if ( $IsDifferentValue )
+                        {
+                            $pass=$false
+                        }
+                        return $pass
+                    }
                 }
-
             }
             It "Verify overide parameter ""<parameter>"" with value ""<value>""" -ForEach @(
                 @{ Parameter = "M365Environment";       Value = "gcc"                           }
